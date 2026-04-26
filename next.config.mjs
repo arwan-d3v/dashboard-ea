7736b55c7383df6a8c9ff1d167f6a36156d2a509
arwan-d3v <arwan.d3v@gmail.com>
2026-04-26 21:32:13 +0800

    Update konfigurasi untuk Static Export Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Baris ini yang akan menyulap Next.js menjadi Static HTML
+  output: 'export',
+  
+  // Menonaktifkan optimalisasi gambar bawaan server karena kita pakai static
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
